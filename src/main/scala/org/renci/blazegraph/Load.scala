ba4296e5c508a76828b779954b0945cc68db7da4
Jim Balhoff <jim@balhoff.org>
2017-07-28 10:23:54 -0400

    Implemented option to use ontology IRI for named graph to load.

--- src/main/scala/org/renci/blazegraph/Load.scala
+++ src/main/scala/org/renci/blazegraph/Load.scala
@@ -1,15 +1,21 @@
 package org.renci.blazegraph
 
 import java.io.File
+import java.io.FileInputStream
 
 import org.backuity.clist._
+import org.openrdf.model.BNode
+import org.openrdf.model.Statement
 import org.openrdf.rio.RDFFormat
+import org.openrdf.rio.Rio
+import org.openrdf.rio.helpers.RDFHandlerBase
 
 import com.bigdata.rdf.sail.BigdataSailRepositoryConnection
 
 object Load extends Command(description = "Load triples") with Common with GraphSpecific {
 
   var base = opt[String](default = "")
+  var useOntologyGraph = opt[Boolean](default = false, name = "use-ontology-graph")
   var data = arg[File]()
 
   def inputFormat: RDFFormat = informat.getOrElse("turtle") match {
@@ -21,7 +27,8 @@ object Load extends Command(description = "Load triples") with Common with Graph
 
   def runUsingConnection(blazegraph: BigdataSailRepositoryConnection): Unit = {
     val factory = blazegraph.getValueFactory
-    val graph = graphOpt.map(factory.createURI).getOrElse(null)
+    val ontGraphOpt = if (useOntologyGraph) findOntologyURI else None
+    val graph = ontGraphOpt.orElse(graphOpt).map(factory.createURI).getOrElse(null)
     val mutationCounter = new MutationCounter()
     blazegraph.addChangeLog(mutationCounter)
     blazegraph.begin()
@@ -31,5 +38,35 @@ object Load extends Command(description = "Load triples") with Common with Graph
     blazegraph.removeChangeLog(mutationCounter)
     logger.info(s"$mutations changes")
   }
+
+  /**
+   * Tries to efficiently find the ontology IRI triple without loading the whole file.
+   */
+  def findOntologyURI: Option[String] = {
+    object Handler extends RDFHandlerBase {
+      override def handleStatement(statement: Statement): Unit = if (statement.getObject.stringValue == "http://www.w3.org/2002/07/owl#Ontology" &&
+        statement.getPredicate.stringValue == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type") throw new FoundTripleException(statement)
+    }
+    val inputStream = new FileInputStream(data)
+    try {
+      val parser = Rio.createParser(inputFormat)
+      parser.setRDFHandler(Handler)
+      parser.parse(inputStream, base)
+      // If an ontology IRI triple is found, it will be thrown out
+      // in an exception. Otherwise, return None.
+      None
+    } catch {
+      case FoundTripleException(statement) => {
+        if (statement.getSubject.isInstanceOf[BNode]) {
+          logger.warn(s"Blank node subject for ontology triple: $statement")
+          None
+        } else Option(statement.getSubject.stringValue)
+      }
+    } finally {
+      inputStream.close()
+    }
+  }
+
+  final case class FoundTripleException(statement: Statement) extends RuntimeException
 
 }
