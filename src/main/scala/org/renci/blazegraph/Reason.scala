a840586ff9aa9f75e24ccc4213a6171556add41c
Jim Balhoff <jim@balhoff.org>
2017-08-11 12:46:42 -0400

    Changes to reason command CLI.

--- src/main/scala/org/renci/blazegraph/Reason.scala
+++ src/main/scala/org/renci/blazegraph/Reason.scala
@@ -9,7 +9,9 @@ import scala.concurrent.Future
 import scala.concurrent.blocking
 import scala.concurrent.duration.Duration
 import scala.util.Failure
+import scala.util.control.NonFatal
 
+import org.apache.jena.reasoner.rulesys.Rule
 import org.apache.jena.system.JenaSystem
 import org.backuity.clist._
 import org.geneontology.jena.OWLtoRules
@@ -17,8 +19,15 @@ import org.geneontology.rules.engine.RuleEngine
 import org.geneontology.rules.util.Bridge
 import org.openrdf.model.Statement
 import org.openrdf.model.impl.URIImpl
+import org.openrdf.query.QueryLanguage
 import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.formats.RioRDFXMLDocumentFormatFactory
+import org.semanticweb.owlapi.model.IRI
+import org.semanticweb.owlapi.model.OWLOntology
+import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration
 import org.semanticweb.owlapi.model.parameters.Imports
+import org.semanticweb.owlapi.rio.RioMemoryTripleSource
+import org.semanticweb.owlapi.rio.RioParserImpl
 
 import com.bigdata.rdf.sail.BigdataSailRepository
 import com.bigdata.rdf.sail.BigdataSailRepositoryConnection
@@ -30,36 +39,62 @@ import akka.stream.scaladsl._
 
 object Reason extends Command(description = "Materialize inferences") with Common with LazyLogging {
 
-  var appendGraphName = opt[String](default = "#inferred")
-  var targetGraph = opt[Option[String]]()
-  var mergeSources = opt[Boolean](default = false)
-  var ontology = opt[File](default = new File("ro.owl"))
-  var rulesFile = opt[Option[File]]() // Not used yet
-  var parallelism = opt[Int](default = Runtime.getRuntime().availableProcessors)
-  var sourceGraphs = args[List[String]]()
+  var targetGraph = opt[Option[String]](description = "Named graph to store inferred statements.")
+  var appendGraphName = opt[String](default = "#inferred", description = "If a target-graph is not provided, append this text to the end of source graph name to use as target graph for inferred statements.")
+  var mergeSources = opt[Boolean](default = false, description = "Merge all selected source graphs into one set of statements before reasoning. Inferred statements will be stored in provided `target-graph`, or else in the default graph. If `merge-sources` is false (default), source graphs will be reasoned separately and in parallel.")
+  var ontology = opt[Option[String]](description = "Ontology to use as rule source. If the passed value is a valid filename, the ontology will be read from the file. Otherwise, if the value is an ontology IRI, it will be loaded from the database if such a graph exists, or else, from the web.")
+  var rulesFile = opt[Option[File]](description = "Reasoning rules in Jena syntax.")
+  var parallelism = opt[Int](default = Math.min(Runtime.getRuntime().availableProcessors / 2, 2), description = "Maximum graphs to simultaneously either read from database or run reasoning on.")
+  var sourceGraphs = arg[String](description = "File name or query text of SPARQL select used to obtain graph names on which to perform reasoning. The query must return a column named `source_graph`.")
 
-  private def computedTargetGraph(graph: String): String = targetGraph.getOrElse {
-    if (mergeSources) inferredGraphName(sourceGraphs.head)
-    else inferredGraphName(graph)
+  private def computedTargetGraph(graph: String): Option[String] = targetGraph.orElse {
+    if (mergeSources) None
+    else Option(inferredGraphName(graph))
   }
 
   private def inferredGraphName(graph: String): String = s"$graph$appendGraphName"
 
   def runUsingConnection(blazegraph: BigdataSailRepositoryConnection): Unit = {
     JenaSystem.init()
+
+    val ontRules = ontology.flatMap(tryLoadOntologyFromFile)
+      .orElse(ontology.flatMap(tryLoadOntologyFromDatabase(_, blazegraph.getRepository)))
+      .orElse(ontology.flatMap(tryLoadOntologyFromWeb))
+      .map(ont => Bridge.rulesFromJena(OWLtoRules.translate(ont, Imports.INCLUDED, true, true, false, true)).toSet)
+      .getOrElse(Set.empty)
+    val extraRules = rulesFile.map(file => Bridge.rulesFromJena(Rule.parseRules(io.Source.fromFile(file, "utf-8").mkString).asScala).toSet).getOrElse(Set.empty)
+    val allRules = ontRules ++ extraRules
+    if (allRules.isEmpty) {
+      logger.error(s"No rules provided.")
+      System.exit(1)
+    }
     implicit val system = ActorSystem("Reason")
     implicit val materializer = ActorMaterializer()
-    val ont = OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(ontology)
-    val arachne = new RuleEngine(Bridge.rulesFromJena(OWLtoRules.translate(ont, Imports.INCLUDED, true, true, true, true)), false)
-    ont.getOWLOntologyManager.removeOntology(ont)
-    val repository = blazegraph.getRepository
-    val sourceGraphGroups = if (mergeSources) List(sourceGraphs -> computedTargetGraph(sourceGraphs.head))
-    else sourceGraphs.map(g => List(g) -> computedTargetGraph(g))
+    val arachne = new RuleEngine(allRules, false)
+
+    val sourcesQueryString = if (new File(sourceGraphs).exists) io.Source.fromFile(sourceGraphs, "utf-8").mkString else sourceGraphs
+    val query = blazegraph.prepareTupleQuery(QueryLanguage.SPARQL, sourcesQueryString)
+    val sourcesQueryResult = query.evaluate()
+    var sourceGraphNames = List.empty[String]
+    while (sourcesQueryResult.hasNext()) {
+      sourceGraphNames = sourcesQueryResult.next().getValue("source_graph").stringValue :: sourceGraphNames
+    }
+    logger.info(s"Reasoning on source graphs: \n${sourceGraphNames.mkString("\n")}")
+    sourcesQueryResult.close()
+    val sourceGraphGroups = if (mergeSources) List(sourceGraphNames -> computedTargetGraph(sourceGraphNames.head))
+    else sourceGraphNames.map(g => List(g) -> computedTargetGraph(g))
+
+    // Now process each graph group in 3 stages: 
+    // (1) load graph group statements from database 
+    // (2) run reasoner over statements
+    // (3) insert inferred statements into database
+    // Each of these will happen in parallel up to the maximum 'parallelism' value, but early stages will wait
+    // to start a new graph group if downstream stages are busy.
     val done = Source(sourceGraphGroups)
       .mapAsyncUnordered(parallelism) {
         case (graphs, targetGraph) =>
           logger.debug(s"Loading from $graphs for target $targetGraph")
-          statementsForGraphs(graphs, repository).map((_, targetGraph))
+          statementsForGraphs(graphs, blazegraph.getRepository).map((_, targetGraph))
       }
       .mapAsyncUnordered(parallelism) {
         case (statements, targetGraph) => Future {
@@ -67,20 +102,19 @@ object Reason extends Command(description = "Materialize inferences") with Commo
           val triples = statements.map(ArachneBridge.createTriple)
           val wm = arachne.processTriples(triples)
           val inferred = wm.facts -- wm.asserted
-          val res = (inferred.map(ArachneBridge.createStatement(blazegraph.getValueFactory, _)), targetGraph)
+          val result = (inferred.map(ArachneBridge.createStatement(blazegraph.getValueFactory, _)), targetGraph)
           logger.debug(s"Done reasoning $targetGraph")
-          res
+          result
         }
       }
       .runForeach {
         case (statements, targetGraph) =>
-          logger.debug(s"Runforeach $targetGraph")
+          logger.debug(s"Inserting result into $targetGraph")
           blocking {
-            logger.debug(s"Inserting result into $targetGraph")
             val mutationCounter = new MutationCounter()
             blazegraph.addChangeLog(mutationCounter)
             blazegraph.begin()
-            blazegraph.add(statements.asJava, new URIImpl(targetGraph))
+            blazegraph.add(statements.asJava, targetGraph.map(new URIImpl(_)).get)
             blazegraph.commit()
             val mutations = mutationCounter.mutationCount
             blazegraph.removeChangeLog(mutationCounter)
@@ -96,7 +130,34 @@ object Reason extends Command(description = "Materialize inferences") with Commo
     }
   }
 
-  def statementsForGraphs(graphs: Seq[String], repository: BigdataSailRepository): Future[Set[Statement]] = Future {
+  private def tryLoadOntologyFromFile(filename: String): Option[OWLOntology] = try {
+    val file = new File(filename)
+    if (file.exists) Option(OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(file)) else None
+  } catch {
+    case NonFatal(e) => None
+  }
+
+  private def tryLoadOntologyFromDatabase(graphName: String, repository: BigdataSailRepository): Option[OWLOntology] = try {
+    val manager = OWLManager.createOWLOntologyManager()
+    val statements = Await.result(statementsForGraphs(List(graphName), repository), Duration.Inf)
+    val source = new RioMemoryTripleSource(statements.asJava)
+    val parser = new RioParserImpl(new RioRDFXMLDocumentFormatFactory())
+    val newOnt = manager.createOntology()
+    //TODO what will happen with imports? Ignored, or loaded from web??
+    parser.parse(source, newOnt, new OWLOntologyLoaderConfiguration())
+    Option(newOnt)
+  } catch {
+    case NonFatal(e) => None
+  }
+
+  private def tryLoadOntologyFromWeb(ontIRI: String): Option[OWLOntology] = try {
+    val manager = OWLManager.createOWLOntologyManager()
+    Option(manager.loadOntology(IRI.create(ontIRI)))
+  } catch {
+    case NonFatal(e) => None
+  }
+
+  private def statementsForGraphs(graphs: Seq[String], repository: BigdataSailRepository): Future[Set[Statement]] = Future {
     blocking {
       val connection = repository.getReadOnlyConnection
       val result = connection.getStatements(null, null, null, false, graphs.map(new URIImpl(_)): _*)
