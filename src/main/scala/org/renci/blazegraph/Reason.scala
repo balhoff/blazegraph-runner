d929d3ff0a122579633b5bbbd50053f51c7c1c74
Jim Balhoff <jim@balhoff.org>
2020-02-03 21:25:20 -0500

    Add Whelk as reasoner choice. Fixes #12.

--- src/main/scala/org/renci/blazegraph/Reason.scala
+++ src/main/scala/org/renci/blazegraph/Reason.scala
@@ -11,15 +11,18 @@ import org.apache.jena.sys.JenaSystem
 import org.backuity.clist._
 import org.geneontology.jena.OWLtoRules
 import org.geneontology.rules.engine.{RuleEngine, Triple}
-import org.geneontology.rules.util.Bridge
+import org.geneontology.rules.util.{Bridge => RulesBridge}
+import org.geneontology.whelk.{AtomicConcept, BuiltIn, ConceptAssertion, ConceptInclusion, ExistentialRestriction, Individual, Nominal, Reasoner, Role, RoleAssertion, Bridge => WhelkBridge}
 import org.openrdf.model.impl.URIImpl
-import org.openrdf.model.{Statement, URI}
+import org.openrdf.model.vocabulary.RDF
+import org.openrdf.model.{Statement, URI, ValueFactory}
 import org.openrdf.query.QueryLanguage
 import org.semanticweb.owlapi.apibinding.OWLManager
 import org.semanticweb.owlapi.formats.RioRDFXMLDocumentFormatFactory
 import org.semanticweb.owlapi.model.parameters.Imports
 import org.semanticweb.owlapi.model.{IRI, OWLOntology, OWLOntologyLoaderConfiguration}
 import org.semanticweb.owlapi.rio.{RioMemoryTripleSource, RioParserImpl}
+import org.semanticweb.owlapi.vocab.OWLRDFVocabulary
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
@@ -38,6 +41,7 @@ object Reason extends Command(description = "Materialize inferences") with Commo
   var parallelism = opt[Int](default = Math.max(Runtime.getRuntime().availableProcessors / 2, 2), description = "Maximum graphs to simultaneously either read from database or run reasoning on.")
   var sourceGraphsQuery = opt[Option[String]](description = "File name or query text of SPARQL select used to obtain graph names on which to perform reasoning. The query must return a column named `source_graph`.")
   var sourceGraphs = opt[Option[String]](description = "Space-separated graph IRIs on which to perform reasoning (must be passed as one shell argument).")
+  var reasonerChoice = opt[String](name = "reasoner", default = "arachne", description = "Reasoner choice: 'arachne' (default) or 'whelk'")
 
   private val ProvDerivedFrom = new URIImpl("http://www.w3.org/ns/prov#wasDerivedFrom")
 
@@ -51,27 +55,24 @@ object Reason extends Command(description = "Materialize inferences") with Commo
   def runUsingConnection(blazegraph: BigdataSailRepositoryConnection): Unit = {
     JenaSystem.init()
 
-    val ontRules = ontology.flatMap(tryLoadOntologyFromFile)
+    val maybeOnt = ontology.flatMap(tryLoadOntologyFromFile)
       .orElse(ontology.flatMap(tryLoadOntologyFromDatabase(_, blazegraph.getRepository)))
       .orElse(ontology.flatMap(tryLoadOntologyFromWeb))
-      .map(ont => Bridge.rulesFromJena(OWLtoRules.translate(ont, Imports.INCLUDED, true, true, false, true)).toSet)
-      .getOrElse(Set.empty)
-    val extraRules = rulesFile.map(file => Bridge.rulesFromJena(Rule.parseRules(io.Source.fromFile(file, "utf-8").mkString).asScala).toSet).getOrElse(Set.empty)
-    val allRules = ontRules ++ extraRules
-    if (allRules.isEmpty) {
-      scribe.error(s"No rules provided.")
-      System.exit(1)
+    val factory = blazegraph.getValueFactory
+    val reasoner = reasonerChoice.toLowerCase match {
+      case "arachne" => new ArachneReasoner(maybeOnt, rulesFile.map(f => io.Source.fromFile(f, "utf-8").mkString), factory)
+      case "whelk"   => new WhelkReasoner(maybeOnt.getOrElse(OWLManager.createOWLOntologyManager().createOntology()), factory)
+      case _         => throw new IllegalArgumentException(s"Invalid reasoner choice: $reasonerChoice")
     }
     implicit val system = ActorSystem("Reason")
     implicit val materializer = ActorMaterializer()
-    val arachne = new RuleEngine(allRules, false)
 
     var sourceGraphNames = sourceGraphs.map(_.split(" ", -1)).toList.flatten
     sourceGraphsQuery.foreach { sgQuery =>
       val sourcesQueryString = if (new File(sgQuery).exists) io.Source.fromFile(sgQuery, "utf-8").mkString else sgQuery
       val query = blazegraph.prepareTupleQuery(QueryLanguage.SPARQL, sourcesQueryString)
       val sourcesQueryResult = query.evaluate()
-      while (sourcesQueryResult.hasNext()) {
+      while (sourcesQueryResult.hasNext) {
         val bindingSet = sourcesQueryResult.next()
         if (bindingSet.hasBinding("source_graph")) sourceGraphNames = bindingSet.getValue("source_graph").stringValue :: sourceGraphNames
         else {
@@ -82,7 +83,7 @@ object Reason extends Command(description = "Materialize inferences") with Commo
       }
       sourcesQueryResult.close()
     }
-    scribe.info(s"Reasoning on source graphs: \n${sourceGraphNames.mkString("\n")}")
+    scribe.debug(s"Reasoning on source graphs: \n${sourceGraphNames.mkString("\n")}")
 
     val sourceGraphGroups = if (mergeSources) List(sourceGraphNames -> computedTargetGraph(sourceGraphNames.head))
     else sourceGraphNames.map(g => List(g) -> computedTargetGraph(g))
@@ -109,27 +110,24 @@ object Reason extends Command(description = "Materialize inferences") with Commo
             targetGraph <- maybeTargetGraph
           } yield factory.createStatement(targetGraph, ProvDerivedFrom, sourceGraph)
           scribe.debug(s"Reasoning for $maybeTargetGraph")
-          val triples = statements.map(ArachneBridge.createTriple)
-          val wm = arachne.processTriples(triples)
-          val inferred: Set[Triple] = wm.facts.toSet -- wm.asserted
-          val result = (inferred.map(ArachneBridge.createStatement(factory, _)) ++ provenanceStatements,
-            maybeTargetGraph)
+          val inferred = reasoner.computeInferences(statements)
+          val result = (inferred ++ provenanceStatements, maybeTargetGraph)
           scribe.debug(s"Done reasoning $maybeTargetGraph")
           result
         }
       }
       .runForeach {
-        case (statements, maybeTargetGraph) =>
-          scribe.debug(s"Inserting result into $maybeTargetGraph")
+        case (statements, Some(targetGraph)) =>
+          scribe.debug(s"Inserting result into $targetGraph")
           blocking {
             val mutationCounter = new MutationCounter()
             blazegraph.addChangeLog(mutationCounter)
             blazegraph.begin()
-            blazegraph.add(statements.asJava, maybeTargetGraph.get)
+            blazegraph.add(statements.asJava, targetGraph)
             blazegraph.commit()
             val mutations = mutationCounter.mutationCount
             blazegraph.removeChangeLog(mutationCounter)
-            scribe.info(s"$mutations changes in $maybeTargetGraph")
+            scribe.info(s"$mutations changes in $targetGraph")
           }
       }
     Await.ready(done, Duration.Inf).onComplete {
@@ -148,9 +146,14 @@ object Reason extends Command(description = "Materialize inferences") with Commo
     case NonFatal(e) => None
   }
 
-  private def tryLoadOntologyFromDatabase(graphName: String, repository: BigdataSailRepository): Option[OWLOntology] = try {
+  private def tryLoadOntologyFromDatabase(graphName: String, repository: BigdataSailRepository): Option[OWLOntology] = {
     val manager = OWLManager.createOWLOntologyManager()
     val statements = Await.result(statementsForGraphs(List(new URIImpl(graphName)), repository), Duration.Inf)
+    ontologyFromStatements(statements)
+  }
+
+  private def ontologyFromStatements(statements: Set[Statement]): Option[OWLOntology] = try {
+    val manager = OWLManager.createOWLOntologyManager()
     if (statements.nonEmpty) {
       val source = new RioMemoryTripleSource(statements.asJava)
       val parser = new RioParserImpl(new RioRDFXMLDocumentFormatFactory())
@@ -180,6 +183,70 @@ object Reason extends Command(description = "Materialize inferences") with Commo
       connection.close()
       statements
     }
+  }
+
+  private sealed trait BGReasoner {
+
+    def computeInferences(statements: Set[Statement]): Set[Statement]
+
+  }
+
+  private class ArachneReasoner(ontology: Option[OWLOntology], rules: Option[String], factory: ValueFactory) extends BGReasoner {
+
+    private val ontRules = ontology.toSet[OWLOntology].flatMap(ont => RulesBridge.rulesFromJena(OWLtoRules.translate(ont, Imports.INCLUDED, true, true, false, true)).toSet)
+    private val extraRules = rules.toSet[String].flatMap(rs => RulesBridge.rulesFromJena(Rule.parseRules(rs).asScala).toSet)
+    private val allRules = ontRules ++ extraRules
+    private val arachne = new RuleEngine(allRules, false)
+
+    def computeInferences(statements: Set[Statement]): Set[Statement] = {
+      val triples = statements.map(ArachneBridge.createTriple)
+      val wm = arachne.processTriples(triples)
+      val inferred: Set[Triple] = wm.facts.toSet -- wm.asserted
+      inferred.map(ArachneBridge.createStatement(factory, _))
+    }
+
+  }
+
+  private class WhelkReasoner(ontology: OWLOntology, factory: ValueFactory) extends BGReasoner {
+
+    private val tbox = WhelkBridge.ontologyToAxioms(ontology)
+    private val whelk = Reasoner.assert(tbox)
+    private val tboxClassAssertions = whelk.classAssertions
+    private val tboxRoleAssertions = whelk.roleAssertions
+    //TODO this should possibly be part of the whelk API
+    //ObjectProperty declarations are needed for correct parsing of OWL properties from RDF
+    private val allRoles = tbox.flatMap(_.signature).collect { case role: Role => role }
+    private val propertyDeclarations = allRoles.map(r => factory.createStatement(factory.createURI(r.id), RDF.TYPE, factory.createURI(OWLRDFVocabulary.OWL_OBJECT_PROPERTY.getIRI.toString)))
+
+    def computeInferences(statements: Set[Statement]): Set[Statement] = {
+      val statementsWithoutImports = statements.filterNot(_.getPredicate.stringValue == OWLRDFVocabulary.OWL_IMPORTS.getIRI.toString)
+      ontologyFromStatements(statementsWithoutImports ++ propertyDeclarations) match {
+        case Some(ont) =>
+          val axioms = WhelkBridge.ontologyToAxioms(ont).collect { case ci: ConceptInclusion => ci }
+          val assertedClassAssertions = (for {
+            ConceptInclusion(Nominal(ind), ac @ AtomicConcept(_)) <- axioms
+          } yield ConceptAssertion(ac, ind))
+          val assertedRoleAssertions = (for {
+            ConceptInclusion(Nominal(subject), ExistentialRestriction(role @ Role(_), Nominal(target))) <- axioms
+          } yield RoleAssertion(role, subject, target))
+          val updated = Reasoner.assert(axioms, whelk)
+          val newClassAssertions = (updated.classAssertions -- assertedClassAssertions -- tboxClassAssertions)
+            .filterNot(_.concept == BuiltIn.Top)
+          val newRoleAssertions = updated.roleAssertions -- assertedRoleAssertions -- tboxRoleAssertions
+          newClassAssertions.flatMap(ca => classAssertionToStatement(ca, factory).toSet) ++ newRoleAssertions.map(roleAssertionToStatement(_, factory))
+        case None      =>
+          scribe.error("Couldn't create OWL ontology from RDF statements")
+          Set.empty
+      }
+    }
+
+    private def classAssertionToStatement(ca: ConceptAssertion, factory: ValueFactory): Option[Statement] = ca match {
+      case ConceptAssertion(AtomicConcept(c), Individual(ind)) => Some(factory.createStatement(factory.createURI(ind), RDF.TYPE, factory.createURI(c)))
+      case _                                                   => None
+    }
+
+    private def roleAssertionToStatement(ra: RoleAssertion, factory: ValueFactory): Statement = factory.createStatement(factory.createURI(ra.subject.id), factory.createURI(ra.role.id), factory.createURI(ra.target.id))
+
   }
 
 }
