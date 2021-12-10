b9127d79eddc6bdc628f5916ddcab92c4b42de06
Jim Balhoff <jim@balhoff.org>
2021-12-10 12:43:12 -0500

    Use export API for dump command.

--- src/main/scala/org/renci/blazegraph/Dump.scala
+++ src/main/scala/org/renci/blazegraph/Dump.scala
@@ -1,21 +1,19 @@
 package org.renci.blazegraph
 
-import java.io.{BufferedOutputStream, File, FileOutputStream}
-
 import com.bigdata.rdf.sail.BigdataSailRepositoryConnection
 import org.backuity.clist._
-import org.openrdf.query.QueryLanguage
+
+import java.io.{BufferedOutputStream, File, FileOutputStream}
 
 object Dump extends Command(description = "Dump Blazegraph database to an RDF file") with Common with RDFOutputting with GraphSpecific {
 
   var output = arg[File]()
 
   def runUsingConnection(blazegraph: BigdataSailRepositoryConnection): Unit = {
-    val graph = graphOpt.map(g => s"FROM <$g>").getOrElse("")
-    val query = blazegraph.prepareGraphQuery(QueryLanguage.SPARQL, s"CONSTRUCT $graph WHERE { ?s ?p ?o . }")
-    val queryOutput = new BufferedOutputStream(new FileOutputStream(output))
-    query.evaluate(createOutputWriter(queryOutput))
-    queryOutput.close()
+    val outputStream = new BufferedOutputStream(new FileOutputStream(output))
+    val writer = createOutputWriter(outputStream)
+    blazegraph.exportStatements(null, null, null, false, writer, graphOpt.map(blazegraph.getValueFactory.createURI).toSeq: _*)
+    outputStream.close()
   }
 
 }
